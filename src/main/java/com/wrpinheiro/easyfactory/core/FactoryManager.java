ba38fdedf78076bf69a9076a5f51bf8377394ec6
Wellington Pinheiro <wellington.pinheiro@gmail.com>
2015-11-11 23:27:37 -0200

    Create some factories to load the factories defs.

--- src/main/java/com/wrpinheiro/easyfactory/core/FactoryManager.java
+++ src/main/java/com/wrpinheiro/easyfactory/core/FactoryManager.java
@@ -1,9 +1,21 @@
 package com.wrpinheiro.easyfactory.core;
 
+import com.wrpinheiro.easyfactory.core.model.Factory;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Wellington Pinheiro <wellington.pinheiro@gmail.com>
  */
 public class FactoryManager {
+    private String factoryFilesPath;
+    private Map<String, Factory> factories;
+
+    public FactoryManager(String factoryFilesPath) {
+        this.factories = new HashMap<>();
+    }
+
     public static <T> T load(String factoryName) {
         return null;
     }
